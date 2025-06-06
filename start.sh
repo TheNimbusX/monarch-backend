662b1a0b91e00fc359cf3477a4c986cdeb850eb1
Denis Grab <166665464+TheNimbusX@users.noreply.github.com>
2025-06-06 16:46:03 +0300

    Initial commit for backend

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+# Убедимся, что Laravel готов
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+# Кэш и ключ
+php artisan config:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Миграции (если нужно)
 php artisan migrate --force
-php artisan serve --host=0.0.0.0 --port=$PORT
+
+# Запуск Laravel-сервера
+php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
